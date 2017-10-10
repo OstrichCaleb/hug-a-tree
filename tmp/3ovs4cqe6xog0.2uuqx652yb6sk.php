bbdede050dc364410662db279bf623bd3009cdb7
ducknguyen <dt.nguyen95@yahoo.com>
2017-10-10 16:11:08 -0500

    login and new-user page updated

---
+++ tmp/3ovs4cqe6xog0.2uuqx652yb6sk.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Duck Nguyen">
+    <meta name="description" content="Duck Nguyen Portfolio Homepage">
+
+    <title>Hug A Tree | Hiking Guide</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+    
+    <script src="https://use.fontawesome.com/b9f1530c0e.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="js/validate.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    
+    <!-- Custom CSS -->
+    <link href="./css/layout.css" rel="stylesheet">
+    <link href="./css/hiking.css" rel="stylesheet">
+</head>
+<body>
+  
+  <?php if ($SESSION['id'] == NULL): ?>
+    <?php echo $this->render('includes/nav.inc.html',NULL,get_defined_vars(),0); ?>
+    <?php else: ?><?php echo $this->render('includes/user-nav.inc.html',NULL,get_defined_vars(),0); ?>
+  <?php endif; ?>
+  <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+  <div class="wrapper">
+		<div class="container">
+      <form action="./submit" method="post" enctype="multipart/form-data" id="location-form" class="form-vertical">
+        <input type="text" name="title" id="title" value="<?= $SESSION['title'] ?>"><label for="title" required>Title</label>
+          <?php if ($SESSION['titleError'] != NULL): ?>
+            
+              <div class="alert-danger col-sm-8">
+                <strong>Error: </strong><span><?= $SESSION['titleError'] ?></span>
+              </div>
+            
+          <?php endif; ?>
+          <div class="alert alert-danger col-sm-8">
+            <strong>Error: </strong><span id="title-error"></span>
+          </div>
+				<input type="text" name="subTitle" id="subTitle" value="<?= $SESSION['subTitle'] ?>"><label for="subTitle" required>Sub Title</label>
+          <?php if ($SESSION['subTitleError'] != NULL): ?>
+            
+              <div class="alert-danger col-sm-8">
+                <strong>Error: </strong><span><?= $SESSION['subTitleError'] ?></span>
+              </div>
+            
+          <?php endif; ?>
+          <div class="alert alert-danger col-sm-8">
+            <strong>Error: </strong><span id="sub-title-error"></span>
+          </div>
+        <textarea rows="4" cols="100%" name="description" id="description" form="location-form"><?= $SESSION['description'] ?></textarea><label for="description">Description</label>
+          <?php if ($SESSION['descriptionError'] != NULL): ?>
+            
+              <div class="alert-danger col-sm-8">
+                <strong>Error: </strong><span><?= $SESSION['descriptionError'] ?></span>
+              </div>
+            
+          <?php endif; ?>
+          <div class="alert alert-danger col-sm-8">
+            <strong>Error: </strong><span id="description-error"></span>
+          </div>
+        <input type="text" name="warning" id="warning" value="<?= $SESSION['warning'] ?>"><label for="warning">Warning</label>
+        <input type="text" name="location" id="location" value="<?= $SESSION['location'] ?>"><label for="location">Location</label>
+          <?php if ($SESSION['locationError'] != NULL): ?>
+            
+              <div class="alert-danger col-sm-8">
+                <strong>Error: </strong><span><?= $SESSION['locationError'] ?></span>
+              </div>
+            
+          <?php endif; ?>
+          <div class="alert alert-danger col-sm-8">
+            <strong>Error: </strong><span id="location-error"></span>
+          </div>
+        <label>Options</label>
+        <?php foreach (($options?:[]) as $option): ?>
+          <input type="checkbox" name="opt[]" value="<?= $option ?>"><?= $option ?><br>
+        <?php endforeach; ?>
+				<label>Type of Activity</label>
+        <?php foreach (($types?:[]) as $type): ?>
+          <input type="checkbox" name="types[]" value="<?= $type ?>"><?= $type ?><br>
+        <?php endforeach; ?>
+        <input id="photo" type="file" name="photo" accept="image/*"/><label for="photo" class="control-label col-sm">Upload Photo</label>
+				<?php if ($SESSION['photoError'] != NULL): ?>
+					
+						<div class="alert-danger col-sm-8">
+							<strong>Error: </strong><span><?= $SESSION['photoError'] ?></span>
+						</div>
+					
+				<?php endif; ?>
+				<div class="alert alert-danger col-sm-8">
+            <strong>Error: </strong><span id="photo-error"></span>
+        </div>
+        <input name="action" type="submit" value="Submit" class="btn">
+      </form>
+    </div>
+  </div>
+	
+	<?php echo $this->render('includes/footer.inc.html',NULL,get_defined_vars(),0); ?>
+    
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
--- tmp/3ovs4cqe6xog0.3i6ikuk6mr8kc.php
+++ tmp/3ovs4cqe6xog0.3i6ikuk6mr8kc.php
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-<!--
----------PHP VALIDATION------------
-
-$dbc = $GLOBALS['dbc'];
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-	$error = false;
-	if (!isset($_POST['email'])){
-		$f3->('SESSION.emailErr', 'Please enter your email address');
-		$error = true;
-	} else if (isset($_POST['email'])) {
-		if(email not in database){
-			$f3->('SESSION.emailErr', 'Email not found. Try again.');
-			$error = true;
-		}
-	}
-	
-	if (!isset($_POST['password'])){
-		$f3->('SESSION.passwordErr', 'Please enter a valid password');
-		$error = true;
-	} else if (isset($_POST['password'])) {
-		if(password not in database){
-			$f3->('SESSION.passErr', 'Password not found. Try again.');
-			$error = true;
-		}
-	}
-	
-	if(!error){
-		//go to user's homepage
-	}
-	else{
-		$f3->reroute('/login');
-	}
-	unset($_POST);
-}
- $f3->reroute('/');
-
--->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -55,76 +16,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 	<script src="http://code.jquery.com/jquery.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     
-    <!-- Custom CSS -->
-	<!-- YO DEL ONE DOT TO MAKE CSS, IMAGES, JS WORK WITH F3 -->
-	<!-- YO DEL ONE DOT TO MAKE CSS, IMAGES, JS WORK WITH F3 -->
-	<!-- YO DEL ONE DOT TO MAKE CSS, IMAGES, JS WORK WITH F3 -->
-	<!-- YO DEL ONE DOT TO MAKE CSS, IMAGES, JS WORK WITH F3 -->
-	<!-- YO DEL ONE DOT TO MAKE CSS, IMAGES, JS WORK WITH F3 -->
-    <link href="../css/layout.css" rel="stylesheet">
-    <link href="../css/login.css" rel="stylesheet">
+    <link href="./css/layout.css" rel="stylesheet">
+    <link href="./css/login.css" rel="stylesheet">
 </head>
 <body>
-    <div class="navbar-fixed-top">
-        <div class="top-wrapper">
-            <ul class="nav navbar-nav pull-right">
-                <li><a href="">ABOUT</a></li> 
-                <li>
-                    <form class="form-inline">
-                    <div class="input-group">
-                        <span class="input-group-addon" id="site-search"><i class="fa fa-search" aria-hidden="true"></i></span>
-                        <input type="text" class="form-control" placeholder="site search" aria-describedby="site-search">
-                    </div>
-                    </form>
-                </li>
-            </ul> 
-        </div>
-        <nav class="navbar navbar-default bottom-wrapper" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
-             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#home-nav">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-            </div>
-            
-            <div class="col-md-12">
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="home-nav">
-                    <ul class="nav navbar-nav">
-                        <li id="logo"><a href="./"><img src="../images/hugatree.png" alt=""></a></li>
-                        <li><a href="./hiking">HIKING</a></li>
-                        <li><a href="./biking">BIKING</a></li>
-                        <li><a href="./chilling">CHILLING</a></li>
-                        <li><a href="#">JOIN</a></li>
-                    </ul>
-                    <ul class="nav navbar-nav pull-right">
-                        <li><a href="#">SIGNUP/ POST NEW/ LOGIN</a></li>
-                    </ul>
-                </div>
-                <!-- /.navbar-collapse -->
-            </div>
-        </nav>
-    </div>
+	
+	<?php echo $this->render('includes/nav.inc.html',NULL,get_defined_vars(),0); ?>
 	
 	<div class="container">
         <div class="card card-container">
 			
 			<div class="row">
-				<img class="card-img-top" src="../images/login.png">
+				<img class="card-img-top" src="./images/login.png">
 			</div>
 			
-            <form id="login-form" class="form-signin" method="post" action="#" autocomplete="on">
+            <form id="login-form" class="form-signin" action="./login" method="post" autocomplete="on">
                 <!-- EMAIL SECTION + ERROR MESSAGE -->
-                <input type="text" name="email" id="email" class="form-control" placeholder="Email address" autofocus>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Username" autofocus required>
                 <div class="alert alert-danger">
 					<strong>Error:</strong> <span id="emailErr"></span>
 				</div>
                 
                 <!-- PASSWORD SECTION + ERROR MESSAGE -->
-                <input type="password" name="password" id="password" class="form-control" placeholder="Password">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+
                 <div class="alert alert-danger">
 					<strong>Error:</strong> <span id="passErr"></span>
 				</div>
@@ -137,17 +52,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
                     </label>
                 </div>
                
-                <button class="btn btn-lg btn-primary btn-block btn-signin" id="submit" type="submit">
-                    Sign in
-                </button>
+                <input class="btn btn-lg btn-primary btn-block btn-signin" name="action" id="submit" type="submit" value="Sign in">
+                </input>
+				
 				<!--FORGOT YOUR PASSWORD-->
-				<a href="forgotpassword.php" class="forgot-password">
-					Forgot the password?
-				</a>
+				<div>
+					<a href="forgotpassword.php" class="forgot-password">
+						Forgot the password?
+					</a>
+				</div>
+				
+				
             </form>
         </div><!-- /card-container -->
     </div>
-
+	
+	<?php echo $this->render('includes/footer.inc.html',NULL,get_defined_vars(),0); ?>
     
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
@@ -158,83 +78,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 	<script src="../js/login-validate.js"></script>
 </body>
 </html>
-=======
-<!- Caleb Ostrander
-    Blog Assignment
-/>
-<!DOCTYPE html>
-<html lang="en">
-  
-  
-  <head>
-  
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <title>Login</title>
-  
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
-  <link rel="shortcut icon" href="favicon.ico" />
-  
-  <script src="https://use.fontawesome.com/b9f1530c0e.js"></script> 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-  <!-- Custom CSS -->
-  <link href="./css/layout.css" rel="stylesheet">
-  <link href="./css/index.css" rel="stylesheet">
-  <link rel="stylesheet" href="css/style.css">
-  
-</head>
-<body>
-
-<div class="container-fluid">
-  <div class="row content">
-    
-    <?php echo $this->render('includes/nav.inc.html',NULL,get_defined_vars(),0); ?>
-
-    <div class="col-sm-9">
-      <div class="row">
-        <div class="col-sm-9">
-          <h1>Welcome back!</h1>
-          <h4>Please log in below</h4>
-        </div>
-        <div class="col-sm-3">
-          <img src="#">
-        </div>
-      </div>
-      <div class="row text-center">
-        <div class="col">
-          <form action="./login" method="post" class="form-vertical">
-            <div class="row">
-              <div class="col-sm-6 col-sm-offset-3">
-                <div class="row">
-                  <div class="form-group col-sm-8">
-                    <input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
-                  </div>
-                  <div class="form-group col-sm-4">
-                    <label for="username" class="control-label col-sm">Username</label>
-                  </div>
-                  <div class="form-group col-sm-8">
-                    <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
-                  </div>
-                  <div class="form-group col-sm-4">
-                    <label for="password" class="control-label col-sm">Password</label>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="form-group">
-                <input name="action" type="submit" value="Login" class="btn btn-success">
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<?php echo $this->render('includes/footer.inc.html',NULL,get_defined_vars(),0); ?>
-</body>
-</html>
->>>>>>> 20bf09195575b2ae283025107fe129bea5817e8c
